50e46a974114ac5b7ab604f69671cbe4540142a5
garima94921 <gaarimabothra@gmail.com>
2020-07-03 01:27:38 +0530

    Refactored code

--- CupcakeCorner/Model/Order.swift
+++ CupcakeCorner/Model/Order.swift
@@ -8,59 +8,6 @@
 
 import Foundation
 
-struct OrderStruct: Codable {
-    // MARK: Product Details
-    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
-     var type = 0
-     var quantity = 3
-
-    var specialRequestEnabled = false {
-        didSet {
-            if specialRequestEnabled == false {
-                extraFrosting = false
-                addSprinkles = false
-            }
-        }
-        }
-    var extraFrosting = false
-    var addSprinkles = false
-
-    var cost: Double {
-        // ₹25 per cake
-        var cost = Double(quantity) * 25
-
-        // complicated cakes cost more
-        //cost += (Double(type) / 2)
-
-        // ₹10/cake for extra frosting
-        if extraFrosting {
-            cost += Double(quantity) * 10
-        }
-
-        // ₹5/cake for sprinkles
-        if addSprinkles {
-            cost += Double(quantity) * 5
-        }
-
-        return cost
-    }
-
-    // MARK: Address Details
-    var name = ""
-    var streetAddress = ""
-    var city = ""
-    var zip = ""
-
-    var hasValidAddress: Bool {
-        if name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || streetAddress.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || city.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || zip.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-            return false
-        }
-
-        return true
-    }
-
-}
-
 class Order: ObservableObject, Codable {
     @Published var order = OrderStruct()
 
